ded168598347b271646288d200a3a195ef1b809f
gabrj <gabrjc@gmail.com>
2023-04-29 12:25:34 +0200

    added checkpoint

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -1,15 +1,35 @@
+import torch
 
-#TODO
 class DomainDisentangleExperiment: # See point 2. of the project
     
     def __init__(self, opt):
         raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
 
     def save_checkpoint(self, path, iteration, best_accuracy, total_train_loss):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+        
+        checkpoint = {}
+
+        checkpoint['iteration'] = iteration
+        checkpoint['best_accuracy'] = best_accuracy
+        checkpoint['total_train_loss'] = total_train_loss
+
+        checkpoint['model'] = self.model.state_dict()
+        checkpoint['optimizer'] = self.optimizer.state_dict()
+
+        torch.save(checkpoint, path)
 
     def load_checkpoint(self, path):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+        
+        checkpoint = torch.load(path)
+
+        iteration = checkpoint['iteration']
+        best_accuracy = checkpoint['best_accuracy']
+        total_train_loss = checkpoint['total_train_loss']
+
+        self.model.load_state_dict(checkpoint['model'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+
+        return iteration, best_accuracy, total_train_loss
 
     def train_iteration(self, data):
         raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
