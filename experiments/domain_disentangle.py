8dc82ec1b13e4137422e6231e1395ad71ee8b0aa
taurou <80039469+taurou@users.noreply.github.com>
2023-05-14 01:44:40 +0200

    maybe implemented all we need for the disentangler

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -16,8 +16,31 @@ class DomainDisentangleExperiment: # See point 2. of the project
         for param in self.model.parameters():
             param.requires_grad = True
 
-        # TODO Setup optimization procedure
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt['lr'])
+        #self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt['lr'])
+        # Optimizers for all parts of the network.
+        self.optimizer0 = torch.optim.Adam(list(self.model.feature_extractor.parameters()) + 
+                                           list(self.model.category_encoder.parameters()) + 
+                                           list(self.model.category_classifier.parameters()) , lr=opt['lr'])
+        
+        #we freeze the domain classifier DC and train the disentangler to generate fcs (category specific features)
+        #with the objective to fool the domain classifier
+        self.optimizer1 = torch.optim.Adam(self.model.category_encoder.parameters() , lr=opt['lr'])
+       
+        self.optimizer2 = torch.optim.Adam(list(self.model.feature_extractor.parameters()) + 
+                                           list(self.model.domain_encoder.parameters()) + 
+                                           list(self.model.domain_classifier.parameters()) , lr=opt['lr'])
+        
+        #With a well-trained classifier C, we freeze the parameter weights of C and
+        #train the disentangler to generate fds to confuse the category classifier C
+        self.optimizer3 = torch.optim.Adam(self.model.domain_encoder.parameters, lr=opt['lr'])
+        
+        #maybe the feature extractor is not needed to be optimized because in this step i'm optimizing elsewhere.
+        #  In fact the features coming from the reconstruct should be as similar as possible as the features coming from the feature extractor 
+        self.optimizer4 = torch.optim.Adam( 
+                                            list(self.model.domain_encoder.parameters()) +  
+                                            list(self.model.category_encoder.parameters()) + 
+                                            list(self.model.reconstructor.parameters()) , lr=opt['lr'])
+        
         self.criterion_CEL = torch.nn.CrossEntropyLoss()
         self.criterion_EL = EntropyLoss()
         self.criterion_L2L = L2Loss() 
@@ -37,10 +60,19 @@ class DomainDisentangleExperiment: # See point 2. of the project
         checkpoint['total_train_loss'] = total_train_loss
 
         checkpoint['model'] = self.model.state_dict()
-        checkpoint['optimizer'] = self.optimizer.state_dict()
+        checkpoint['optimizer'] = [self.optimizer0.state_dict(), self.optimizer1.state_dict(), self.optimizer2.state_dict(), self.optimizer3.state_dict(), self.optimizer4.state_dict()]
 
         torch.save(checkpoint, path)
 
+
+    def zero_gradients(self):
+        self.optimizer0.zero_grad()
+        self.optimizer1.zero_grad()
+        self.optimizer2.zero_grad()
+        self.optimizer3.zero_grad()
+        self.optimizer4.zero_grad()
+    
+    
     def load_checkpoint(self, path):
         
         checkpoint = torch.load(path)
@@ -50,12 +82,18 @@ class DomainDisentangleExperiment: # See point 2. of the project
         total_train_loss = checkpoint['total_train_loss']
 
         self.model.load_state_dict(checkpoint['model'])
-        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.optimizer0.load_state_dict(checkpoint['optimizer'][0])
+        self.optimizer1.load_state_dict(checkpoint['optimizer'][1])
+        self.optimizer2.load_state_dict(checkpoint['optimizer'][2])
+        self.optimizer3.load_state_dict(checkpoint['optimizer'][3])
+        self.optimizer4.load_state_dict(checkpoint['optimizer'][4])
 
         return iteration, best_accuracy, total_train_loss
 
     def train_iteration(self, data, targetDomain = False):
 
+        self.zero_gradients()
+        
         if(targetDomain == False):
             x, y = data
             x = x.to(self.device)
@@ -63,34 +101,44 @@ class DomainDisentangleExperiment: # See point 2. of the project
             domain_labels = torch.zeros(len(x)).to(self.device) #TODO must check if this works
 
             category_pred = self.model(x, step = 0)
-            category_loss = self.criterion_CEL(category_pred, y)
-            #TODO Handle and propagate loss
+            category_loss = self.w1*self.criterion_CEL(category_pred, y)
+            category_loss.backward()
+            self.optimizer0.step()
+            self.zero_gradients()
+
         else:
             x, _ = data
             x = x.to(self.device)
             domain_labels = torch.ones(len(x)).to(self.device) #TODO must check if this works
             category_loss = 0
 
-        adv_domain = self.model(x, step = 1)
-        adv_domain_loss = self.criterion_EL(adv_domain)
-        #TODO Handle and propagate loss
+        confuse_domain = self.model(x, step = 1)
+        confuse_domain_loss = -self.w1*self.criterion_EL(confuse_domain)*self.alpha
+        confuse_domain_loss.backward()
+        self.optimizer1.step()
+        self.zero_gradients()
+
+
 
         domain_pred = self.model(x, step = 2)
-        domain_loss = self.criterion_CEL(domain_pred, domain_labels)
-        #TODO Handle and propagate loss
+        domain_loss = self.w2*self.criterion_CEL(domain_pred, domain_labels)
+        domain_loss.backward()
+        self.optimizer2.step()
+        self.zero_gradients()
 
-        adv_category = self.model(x, step = 3)
-        adv_category_loss = self.criterion_EL(adv_category)
-        #TODO Handle and propagate loss
+
+        confuse_category = self.model(x, step = 3)
+        confuse_category_loss = -self.w2*self.criterion_EL(confuse_category)*self.alpha
+        confuse_category_loss.backward()
+        self.optimizer3.step()
+        self.zero_gradients()
 
         Fg, Rfg = self.model(x, step = 4) #return recostructor features (Rfg) and extracted features Fg
-        reconstructor_loss = self.criterion_L2L(Rfg, Fg)
-        #TODO handle loss.
+        reconstructor_loss = self.w3*self.criterion_L2L(Rfg, Fg)
+        reconstructor_loss.backward()
+        self.optimizer4.step()
 
-        loss = loss = self.w1 * (category_loss + adv_domain_loss*self.alpha) + self.w2 * (domain_labels + adv_category_loss*self.alpha) + self.w3 * reconstructor_loss 
-        self.optimizer.zero_grad() #TODO handle loss, if necessary, here. The idea here is to optimize step by step the model. So we must figure out how to handle this. 
-        loss.backward()
-        self.optimizer.step()
+        loss = category_loss + confuse_domain_loss + domain_loss + confuse_category_loss + reconstructor_loss
         return loss.item()
 
 
